5aeb6297179a2df001c20e322b271c2b79e886a4
NguyenTrungTin02 <nguyentrungtin160802@gmail.com>
2023-10-03 19:22:43 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route");
 
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -12,6 +13,16 @@ app.get("/",(req,res)=> {
     res.json({message: "Welcome to contact book application."});
 });
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts",contactsRouter);
 
+app.use((req, res, next) =>{
+    return next(new ApiError(404, "Resource not found"));
+});
+
+
+app.use((err, req, res, next) =>{
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Sever Error",
+    });
+});
 module.exports=app;
